8a242c0319b467576980037adb32c02b712e9310
Joepxnlx <joepblankendaal15@gmail.com>
2026-08-07 19:49:01 +0200

    Add a daily top three selection

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_NAME = 'capitol-pulse-v5';
+const CACHE_NAME = 'capitol-pulse-v6';
 const scopeUrl = self.registration.scope;
 const assetUrl = (path) => new URL(path, scopeUrl).toString();
 const APP_SHELL = [
